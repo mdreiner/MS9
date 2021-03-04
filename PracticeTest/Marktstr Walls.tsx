fa2d3048f751871c948b748c2eec31751e2079e3
unknown <croser@team-dreiner.de>
2021-03-04 14:20:45 +0100

    Added map and Tileset

---
+++ PracticeTest/Marktstr Walls.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Marktstr Walls" tilewidth="32" tileheight="32" tilecount="6720" columns="84">
+ <image source="Practice Sprites/Marktstr_Wall_Tileset.png" width="2688" height="2560"/>
+</tileset>
