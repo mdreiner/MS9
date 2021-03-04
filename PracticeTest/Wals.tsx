020c6f3584e117525f06f749c48acc68d6d3e9a0
unknown <croser@team-dreiner.de>
2021-03-04 15:14:37 +0100

    Map downscaled

---
+++ PracticeTest/Wals.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Wals" tilewidth="32" tileheight="32" tilecount="1680" columns="42">
+ <image source="Marktstr_Wall_TilesetDownScaled.png" width="1344" height="1280"/>
+</tileset>
