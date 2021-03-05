f6ca0e7d0cd2223b2109261420b902339e6a0f51
unknown <croser@team-dreiner.de>
2021-03-05 09:15:46 +0100

    updated map

---
+++ Wall75Turned.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Wall75Turned" tilewidth="32" tileheight="32" tilecount="3780" columns="63">
+ <image source="Walls75.png" width="2016" height="1920"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0946186" y="0" width="31.6972" height="31.8865"/>
+  </objectgroup>
+ </tile>
+</tileset>
