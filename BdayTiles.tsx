164606e7a733ef9a0760587e47992597cffcf19a
unknown <croser@team-dreiner.de>
2021-03-19 16:55:53 +0100

    Bday Retest

---
+++ BdayTiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="BdayTiles" tilewidth="32" tileheight="32" tilecount="7378" columns="119">
+ <image source="../MS9/BDay.png" width="3820" height="2000"/>
+</tileset>
