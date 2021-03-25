7bcae290db7a10bc6c1ba9ac85e6d54b13965d68
unknown <croser@team-dreiner.de>
2021-03-25 10:11:56 +0100

    Remade Map.sjon

--- Wals.tsx
+++ Wals.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="Wals" tilewidth="32" tileheight="32" tilecount="1680" columns="42">
- <image source="../../MS9/walls.png" width="1344" height="1280"/>
+ <image source="../MS9/walls.png" width="1344" height="1280"/>
  <tile id="0">
   <properties>
    <property name="collides" type="bool" value="true"/>
